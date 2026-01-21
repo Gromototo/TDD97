2d09ec61aaca6e5b8333577ec992c9a419b44c17
Gromototo <rodlpd51@gmail.com>
2026-01-21 15:53:11 +0100

    Fix bug where we couldn't submit form again if previous email was already used

--- client.js
+++ client.js
@@ -17,7 +17,4 @@ function signUpHandler(inputObject) {
     submitButton.setCustomValidity(signup.message)
     submitButton.reportValidity();
   }
-  else {
-    submitButton.setCustomValidity("");
-  }
 }
