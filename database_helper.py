e3e07b8505f3d3cc0782b78d24c354fabbf8c6c1
Sasse0312 <sasse0312@gmail.com>
2026-01-29 10:17:30 +0100

    step2

--- database_helper.py
+++ database_helper.py
@@ -1,14 +1,36 @@
-DATABASENAME = 'database.db'
+import sqlite3
 
-def getUserByEmail(email):
-  with sqlite3.connect(DATABASENAME) as users:
-          cursor.execute("SELECT username FROM user WHERE username = ?", (email,))
-          if cursor.fetchone():
+DATABASENAME = "database.db"
+
+def getPasswordByEmail(email):
+    with sqlite3.connect(DATABASENAME) as users:
+        users.row_factory = sqlite3.Row
+        cursor = users.cursor()
+        cursor.execute("SELECT password FROM user WHERE username = ?", (email,))
+        return cursor.fetchone()
                
-def addNewUser() :
-  cursor = users.cursor()
-  with sqlite3.connect(DATABASENAME) as users:
+def getUser(email=None, token=None):
 
-          cursor.execute("INSERT INTO user \
-    (username,password,firstname,lastname,gender,city,country) VALUES (?,?,?,?,?,?,?)", (email, password, firstName, lastName, gender, city, country))
-    users.commit()
+    #Either selects the email or the tokens. If none is defined ze don't query the DB.
+    fieldName =  'username' if email else 'token' if token else None
+    field = email if email else token
+    if not fieldName :
+        return Exception("One parameter should be defined")
+    
+    print(fieldName, field)
+    with sqlite3.connect(DATABASENAME) as users:
+        users.row_factory = sqlite3.Row
+        cursor = users.cursor()
+        cursor.execute("SELECT username FROM user WHERE username = ?", (field,))
+
+        return cursor.fetchone()
+    
+def createUser(email, password, firstName, lastName, gender, city, country):
+    with sqlite3.connect(DATABASENAME) as users:
+        users.row_factory = sqlite3.Row
+        cursor = users.cursor()
+        cursor.execute("INSERT INTO user \
+        (username,password,firstname,lastname,gender,city,country) VALUES (?,?,?,?,?,?,?)", (email, password, firstName, lastName, gender, city, country))
+        users.commit()        
+
+               
